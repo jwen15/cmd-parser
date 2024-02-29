6fbe55a7409a83ba27381f292a69bed1172701fe
wenjian <wenjian@bjtu.edu.cn>
2024-02-29 17:50:55 +0800

    test: remove unnecessary while loop

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
  * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
  */
 #include "cmd.h"
+#include <stdio.h>
 
 void test1_cmd(void)
 {
@@ -22,22 +23,18 @@ void test3_cmd(void)
     printf("test3_cmd\n");
 }
 
-REGISTER_CMD(test1, test1_cmd,test1 demo);
-REGISTER_CMD(test2, test2_cmd,test2 demo);
-REGISTER_CMD(test3, test3_cmd,test3 demo);
+REGISTER_CMD(test1, test1_cmd, test1 demo);
+REGISTER_CMD(test2, test2_cmd, test2 demo);
+REGISTER_CMD(test3, test3_cmd, test3 demo);
 
 int main(void)
 {
     cmd_init();
 
-    while (1)
-    {
-        cmd_parsing("_list");
-        cmd_parsing("test1");
-        cmd_parsing("test2");
-        cmd_parsing("test3");
-        delay(1000);
-    }
-    
+    cmd_parsing("_list");
+    cmd_parsing("test1");
+    cmd_parsing("test2");
+    cmd_parsing("test3");
+
     return 0;
 }
